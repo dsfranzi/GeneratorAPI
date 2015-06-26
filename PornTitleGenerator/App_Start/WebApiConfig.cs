6823fdad4979d3dfe7ed696f017cd435308717b5
Franz Wimmer <info@codefoundry.de>
2015-06-26 10:37:44 +0200

    Projekt

---
+++ PornTitleGenerator/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace PornTitleGenerator
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web-API-Konfiguration und -Dienste
+
+            // Web-API-Routen
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+        }
+    }
+}
---
+++ PornTitleGenerator/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace PornTitleGenerator.Controllers
+{
+    public class DefaultController : ApiController
+    {
+
+        public Task<string> GetHelloWorld()
+        {
+            return Task.FromResult("Hello World");
+        }
+
+    }
+}
